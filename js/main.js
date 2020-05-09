102c38c52052da88b473234537d5cd4833f7c8f0
YamazakiSunao <49880780+YamazakiSunao@users.noreply.github.com>
2020-05-09 17:38:45 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function($) {
+    $(function() {
+        var $header = $('#top-head');
+        // Nav Fixed
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 350) {
+                $header.addClass('fixed');
+            } else {
+                $header.removeClass('fixed');
+            }
+        });
+        // Nav Toggle Button
+        $('#nav-toggle').click(function(){
+            $header.toggleClass('open');
+        });
+    });
+})(jQuery);
+
+$(function(){
+    $('a[href^=#]').click(function(){
+        var adjust = -100;
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top + adjust;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
